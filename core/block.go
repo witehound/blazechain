59a265a56a0abdc914c145cd485fd38274973efa
witehound <netvid00@gmail.com>
2023-02-24 20:41:49 +0100

    completed function to decode an encode headder sent in blocks

--- core/block.go
+++ core/block.go
@@ -1,16 +1,56 @@
 package core
 
-import "github.com/witehound/blazechain/types"
+import (
+	"encoding/binary"
+	"io"
+
+	"github.com/witehound/blazechain/types"
+)
 
 type Header struct {
 	Version   uint32
 	Prevlock  types.Hash
 	TimeStamp uint64
 	Height    uint32
-	nonce     uint64
+	Nonce     uint64
 }
 
 type Block struct {
 	Header
 	Transactions []Transaction
+}
+
+func (h *Header) EncodeBinary(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.Prevlock); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.TimeStamp); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+
+	return binary.Write(w, binary.LittleEndian, &h.Nonce)
+}
+
+func (h *Header) DecodeBinary(r io.Reader) error {
+	if err := binary.Read(r, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.Prevlock); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.TimeStamp); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+
+	return binary.Read(r, binary.LittleEndian, &h.Nonce)
+
 }
