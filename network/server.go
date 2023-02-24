b3eed84750b83ecb055ba6e066ffa5397c0fc822
witehound <netvid00@gmail.com>
2023-02-24 17:18:15 +0100

    debuging using localTransport as Transport

--- network/server.go
+++ network/server.go
@@ -1,30 +1,52 @@
 package network
 
+import (
+	"fmt"
+	"time"
+)
+
 type ServerOpts struct {
 	Transports []Transport
 }
 
 type Server struct {
 	ServerOpts
-	rpcCh chan RPC
+	rpcCh  chan RPC
+	quitCh chan struct{}
 }
 
 func NewServer(opts ServerOpts) *Server {
 	return &Server{
 		ServerOpts: opts,
 		rpcCh:      make(chan RPC),
+		quitCh:     make(chan struct{}, 1),
 	}
 }
 
-func (s *Server) start() {
-	s.initTranspose()
+func (s *Server) Start() {
+	s.InitTranspose()
+	ticker := time.NewTicker(5 * time.Second)
+
+free:
+	for {
+		select {
+		case rpc := <-s.rpcCh:
+			fmt.Printf("%v", rpc)
+		case <-s.quitCh:
+			break free
+		case <-ticker.C:
+			fmt.Println("reserve every x minutes")
+		}
+	}
+
+	fmt.Println("Server Shutdown")
 }
 
-func (s *Server) initTranspose() {
+func (s *Server) InitTranspose() {
 	for _, tr := range s.Transports {
 		go func(tr Transport) {
 			for rpc := range tr.Consume() {
-
+				s.rpcCh <- rpc
 			}
 		}(tr)
 	}
