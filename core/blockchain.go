72ee4b789fff0dfaad3da08d7f8e3577ed3b0730
witehound <netvid00@gmail.com>
2023-02-25 17:50:29 +0100

    completed function to create blockchain and chnage validator

--- core/blockchain.go
+++ core/blockchain.go
@@ -1,8 +1,27 @@
 package core
 
 type BlockChain struct {
-	Headers []*Header
-	Store   Storage
+	Headers   []*Header
+	Store     Storage
+	Validator Validator
+}
+
+func NewBlockChain() *BlockChain {
+	bc := &BlockChain{
+		Headers: []*Header{},
+	}
+
+	bc.Validator = NewBlockValidator(bc)
+
+	return bc
+}
+
+func (bc *BlockChain) SetValidator(v Validator) {
+	bc.Validator = v
+}
+
+func (bc *BlockChain) AddBlock() error {
+	return nil
 }
 
 func (bc *BlockChain) Height() uint32 {
---
+++ core/validator.go
@@ -0,0 +1,19 @@
+package core
+
+type Validator interface {
+	ValidateBlock(*Block) error
+}
+
+type BlockValidator struct {
+	bc *BlockChain
+}
+
+func NewBlockValidator(bc *BlockChain) *BlockValidator {
+	return &BlockValidator{
+		bc: bc,
+	}
+}
+
+func (bv *BlockValidator) ValidateBlock(b *Block) error {
+	return nil
+}
