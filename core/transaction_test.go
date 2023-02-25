cfd42d471bd94665e3761c1912cc512854a785c7
witehound <netvid00@gmail.com>
2023-02-25 14:59:06 +0100

    completed test for signin transaction

---
+++ core/transaction_test.go
@@ -0,0 +1,22 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/witehound/blazechain/crypto"
+)
+
+func TestSignTx(t *testing.T) {
+	privkey := crypto.GeneratePrivateKey()
+	data := []byte("foo")
+
+	tx := &Transaction{
+		Data: data,
+	}
+
+	assert.Nil(t, tx.SignTx(privkey))
+
+	assert.NotNil(t, tx.Signature)
+
+}
